44e840ab34a09e80763d3dad1d44090f20f3b7ad
Fábio José Araújo <fabioja@gmail.com>
2024-06-22 12:11:39 -0300

    chore: incluido script para inicialização das migrations ao subir o container

--- app/dist/routes.js
+++ app/dist/routes.js
@@ -4,11 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const create_api_key_controller_1 = __importDefault(require("./controllers/create-api-key-controller"));
 const get_italian_surname_maps_controller_1 = __importDefault(require("./controllers/get-italian-surname-maps.controller"));
 const api_Key_validator_1 = require("./middlewares/api-Key-validator");
 const routes = (0, express_1.Router)();
-routes.post("/api-keys", api_Key_validator_1.apiKeyValidator, new create_api_key_controller_1.default().handle);
 routes.get("/italian-surname-maps", api_Key_validator_1.apiKeyValidator, new get_italian_surname_maps_controller_1.default().handle);
 exports.default = routes;
 //# sourceMappingURL=routes.js.map
